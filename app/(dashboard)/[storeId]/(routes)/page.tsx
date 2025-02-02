dd72f82014342870ea0cede8f961a5a6adc57496
lazytech614 <derupanjan2021@gmail.com>
2025-02-02 20:45:11 +0530

    crete overview page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { 
+  CreditCardIcon, 
+  DollarSignIcon, 
+  PackageIcon 
+} from 'lucide-react'
 
-import prismadb from '@/lib/prismadb'
+import { Heading } from '@/components/ui/heading'
+import { Separator } from '@/components/ui/separator'
+import { 
+  Card, 
+  CardContent, 
+  CardHeader, 
+  CardTitle 
+} from '@/components/ui/card'
+import { formattorPrice } from '@/lib/utils'
+import { getTotalRevenue } from '@/actions/getTotalRevenue'
+import { getSalesCount } from '@/actions/getSalesCount'
+import { getStockCount } from '@/actions/getStockCount'
+import Overview from '@/components/Overview'
+import { getRevenueGraph } from '@/actions/getRevenueGraph'
 
 interface DashboardPageProps {
   params: {
@@ -9,13 +27,73 @@ interface DashboardPageProps {
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({params}) => {
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId
-    }
-  })
+  const totalRevenue = await getTotalRevenue(params.storeId)
+  const salesCount = await getSalesCount(params.storeId)
+  const stockCount = await getStockCount(params.storeId)
+
+  const revenueGraph = await getRevenueGraph(params.storeId)
+
+  console.log(revenueGraph);
+
   return (
-    <div className='px-8'>This is a dashboard of store {store?.name}</div>
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <Heading 
+          title='Dashboard' 
+          description='Overview of your store' 
+        />
+        <Separator />
+        <div className='grid grid-cols-3 gap-3'>
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium capitalize'>
+                Total Revenue
+              </CardTitle>
+              <DollarSignIcon className='h-4 w-4 text-muted-foreground' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>
+                {formattorPrice(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium capitalize'>
+                Sales
+              </CardTitle>
+              <CreditCardIcon className='h-4 w-4 text-muted-foreground' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>
+                {salesCount}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium capitalize'>
+                Products in stock
+              </CardTitle>
+              <PackageIcon className='h-4 w-4 text-muted-foreground' />
+            </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>
+                {stockCount}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className='col-span-4'>
+          <CardHeader>
+            Overview
+          </CardHeader>
+          <CardContent className='pl-2'>
+            <Overview data={revenueGraph} />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   )
 }
 
